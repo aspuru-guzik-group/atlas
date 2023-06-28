1175a2dab6a66904b0dfe2b337eecfc9301f6a2b
rileyhickman <40797414+rileyhickman@users.noreply.github.com>
2023-06-28 10:00:38 -0400

    lots of random changes

---
+++ tests/problem_generator.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+
+# class ProblemGenerator():
+
+#     def __init__(self, problem_type: str):
+#         self.problem_type = problem_type
+
+#         self.accepted_problem_types = ['continuous', ...]
+
+#     def check_problem_type(self):
+#         return self.problem_type in self.accepted_problem_types
+        
+
+#     def generate_instance(self):
+#         """
+#         return surface_callable and campaign 
+
+#         - vary the number of parameters, nnumber of obejctives etc.
+#         - vary the problem itself
+#         - vary the .... 
+
+#         """
+#         if self.problem_type == 'continuous':
+#             # return some surface_callable 
+#             pass
+#         elif self.problem_type == 'discrete':
+#             # return something else
+#             pass 
+
+#         else:
+#             pass 
+
+
+
+
+
+        
+        
