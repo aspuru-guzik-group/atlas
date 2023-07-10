aae38537189c21e1dbea6c92b18e0f1bb8161c24
rileyhickman <40797414+rileyhickman@users.noreply.github.com>
2023-07-10 10:18:49 -0400

    random stuff

---
+++ examples/electrochem_demo/pretty_plotter.py
@@ -0,0 +1,85 @@
+from functools import partial
+from pathlib import Path
+import sys
+
+import numpy as np
+from numpy.typing import NDArray
+from scipy.signal import find_peaks
+from scipy.ndimage import gaussian_filter1d
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def process_cycles(
+    df
+):
+    df = df[-0.75 < df["V"]]
+    df = df[df["V"] < 0.75]
+
+    df["c"] = df["c"].astype(int)
+    # Get Red Ox
+    (_, reduction), (_, oxidation) = df.groupby(np.diff(df["V"].array, append=0) > 0)
+
+    fp_cfg = partial(
+        find_peaks
+        , width=10
+        , distance=10
+        , rel_height=0.9
+    )
+
+    oxi_peaks = map(
+        lambda xs: xs[1][["V", "I"]].iloc[fp_cfg(xs[1]["I"])[0]]
+        , oxidation.groupby("c")
+    )
+    red_peaks = map(
+        lambda xs: xs[1][["V", "I"]].iloc[fp_cfg(-xs[1]["I"])[0]]
+        , reduction.groupby("c")
+    )
+    return tuple(oxi_peaks), tuple(red_peaks)
+
+if __name__ == "__main__":
+    HEADER = ("I", "V", "R", "T", "h", "t", "c")
+    results = pd.read_csv(sys.argv[1], names=HEADER)
+    results = results[results["c"] >= 4]
+    results["I"] *= 1E6
+    results['smoothed_I'] = gaussian_filter1d(results["I"], sigma=2)
+
+    oxi_peaks, _ = process_cycles(results)
+    def checknum(x):
+        y = x["I"].argmax()
+        if (pd.isna(y)): return -1
+        return x.iloc[y]
+
+    oxivalues = pd.concat(
+        map(checknum,oxi_peaks)
+        , axis=1
+        , ignore_index=True
+    ).T
+    max_val = oxivalues.iloc[np.argmax(oxivalues["V"])]
+
+
+
+
+    line = sns.lineplot(
+        data=results
+        , x="V"
+        , y="I"
+        , sort=False
+        , color="#477A7B"
+    )
+    sns.scatterplot(
+        x=max_val["V"]
+        , y=[max_val["I"]]
+        , color="#8F5652"
+        , s=15
+        , marker='X'
+    )
+    line.set(xlabel="Volage / V")
+    line.set(ylabel="Current / μA")
+    fig = line.get_figure()
+    ax = fig.axes[0]
+    # ax.set(ylim=[1.094+0.000650, 1.094+0.000750])
+    fig.set_size_inches(2.4, 1.8)
+    fig.tight_layout()
+    fig.savefig(Path(sys.argv[1]).stem + ".svg")
+    plt.clf()
---
+++ examples/minimal_code_paper/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import pickle
+from olympus import Surface, Campaign
+from atlas.planners.gp.planner import BoTorchPlanner
+
+
+surface = Surface(kind='Branin') # instantiate 2d Branin-Hoo objective function
+
+campaign = Campaign() # define Olympus campaign object 
+campaign.set_param_space(surface.param_space)
+
+planner = BoTorchPlanner(goal='minimize', num_init_design=5) # instantiate GPPlanner 
+planner.set_param_space(surface.param_space)
+
+while len(campaign.observations.get_values()) < 30:
+    samples = planner.recommend(campaign.observations) # ask planner for parameters (list of ParameterVectors)
+    for sample in samples:
+        measurement = surface.run(sample) # measure Branin-Hoo function
+        campaign.add_observation(sample, measurement) # tell planner about most recent observation
+
+
+
+pickle.dump(campaign, open('results.pkl', 'wb'))
