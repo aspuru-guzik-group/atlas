c2c8b606c4e15ae54359f6aeb4c515f6475510ec
rileyhickman <40797414+rileyhickman@users.noreply.github.com>
2023-07-17 12:41:14 -0400

    revisit gp factory

--- src/atlas/acquisition_functions/acqfs.py
+++ src/atlas/acquisition_functions/acqfs.py
@@ -124,8 +124,7 @@ class FeasibilityAwareAcquisition(Object, metaclass=ABCMeta):
 
 
     # helper methods
-
-    def compute_mean_sigma(posterior) -> Tuple[torch.Tensor]:
+    def compute_mean_sigma(self, posterior) -> Tuple[torch.Tensor]:
         """ Takes in posterior of fitted model and returns 
         the mean and covariance """
 
