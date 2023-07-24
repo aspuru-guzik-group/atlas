fa4b04ec6b3a1abe3e3cff6405388f5bc53280de
rileyhickman <40797414+rileyhickman@users.noreply.github.com>
2023-07-24 12:13:27 -0400

    remove general genetic import

--- src/atlas/acquisition_optimizers/__init__.py
+++ src/atlas/acquisition_optimizers/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from .genetic_general_optimizer import GeneticGeneralOptimizer
 from .genetic_optimizer import GeneticOptimizer
 from .gradient_optimizer import GradientOptimizer
 from .pymoo_optimizer import PymooGAOptimizer
